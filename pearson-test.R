89bf729d76d9dd8fa57111b7164f2d4113614da4
rmatsuda5046 <r.matsuda5046@gmail.com>
2025-04-29 11:50:53 +0900

    Delete pearson-test.R

--- pearson-test.R
+++
@@ -1,8 +0,0 @@
-###相関分析
-data <- read.csv("pca-v2.csv", header = TRUE, na.strings = "NA")
-
-#正規性の確認
-shapiro.test(data$L.sha)
-
-#Pearson test
-cor.test(data$Lat, data$diatom.micro.chao, method = "pearson")
